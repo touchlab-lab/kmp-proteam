ce9d01b7e5c1fff19439473fd951cf5a49bc18c8
Kevin Galligan <kgalligan@gmail.com>
2021-12-05 18:04:13 -0500

    log stuff

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import {wait} from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    console.log(`${github.context.action}`)
+    console.log(`github.context.payload.action: ${github.context.payload.action}`)
+    console.log(`github.context.apiUrl: ${github.context.apiUrl}`)
+    console.log(`github.context.ref: ${github.context.ref}`)
+    console.log(`github.context.job: ${github.context.job}`)
+    console.log(`github.context.workflow: ${github.context.workflow}`)
+    console.log(github.context)
     core.debug(`Waiting ${ms} milliseconds ok alright ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
